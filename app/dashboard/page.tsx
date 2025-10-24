dceccf9fa97dad3f964b0eb101a2dca9cd6c4020
Ron Barrantes <ronbarrantes@gmail.com>
2025-10-24 12:03:29 -0400

    Add the most basic of pages

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <div>Menu</div>
+      <div>Dashboard Grid</div>
+      <div></div>
+    </div>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div>
+      <div>Hero</div>
+      <div>Current events</div>
+      <div>Call to action</div>
+    </div>
+  );
+}
