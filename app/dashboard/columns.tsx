693409a4efe2d32011300a09aad71705058402cb
Ron Barrantes <ronbarrantes@gmail.com>
2025-10-24 19:53:37 -0400

    Save progress

---
+++ app/dashboard/columns.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { SportEvent } from "./data-table";
+
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+export const columns: ColumnDef<SportEvent>[] = [
+  { accessorKey: "name", header: "Event Name" },
+  { accessorKey: "sportType", header: "SportType" },
+  { accessorKey: "date", header: "Date" },
+  { accessorKey: "description", header: "Description" },
+  { accessorKey: "venue", header: "Venue" },
+];
