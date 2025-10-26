917b6a2768b625e6158c338c9640a876e3a75a5f
Ron Barrantes <ronbarrantes@gmail.com>
2025-10-26 00:42:51 -0400

    Fixing stuff

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,31 @@
-export default function Login() {
+"use client";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+import { supabase } from "@/lib/supabaseClient";
+
+export default function LoginPage() {
   return (
-    <div>
-      <div>Hero</div>
-      <div>Current events</div>
-      <div>Call to action</div>
-    </div>
+    <Auth
+      supabaseClient={supabase}
+      appearance={{ theme: ThemeSupa }}
+      providers={["google"]} // show Google login button
+    />
   );
 }
+
+// export default function LoginPage() {
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         marginTop: "100px",
+//       }}
+//     >
+//       <h1>Sign in</h1>
+//       <GoogleSignInButton />
+//     </div>
+//   );
+// }
