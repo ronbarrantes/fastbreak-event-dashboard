840e81dcd2681535e54c8b970185a9bb0b5a349b
Ron Barrantes <ronbarrantes@gmail.com>
2025-10-26 19:19:59 -0400

    Create initial action files

---
+++ src/lib/actions/events.ts
@@ -0,0 +1,13 @@
+export async function createEvent() {
+  "use server";
+
+  // Update data
+  // Revalidate cache
+}
+
+export async function deleteEvent() {
+  "use server";
+
+  // Update data
+  // Revalidate cache
+}
