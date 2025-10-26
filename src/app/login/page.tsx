df482caf92b523000e873b0a3a77daab06050011
Ron Barrantes <ronbarrantes@gmail.com>
2025-10-26 00:06:14 -0400

    Update proxy
    Co-authored-by: ronbarrantes <ronbarrantes@gmail.com>

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,52 @@
-export default function Login() {
+"use client";
+import { useEffect } from "react";
+
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useRouter } from "next/navigation";
+
+import { createClient as createBrowserSupabaseClient } from "@/utils/supabase/client";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const supabase = createBrowserSupabaseClient();
+
+  useEffect(() => {
+    // If already signed in, skip login
+    supabase.auth.getSession().then(({ data }) => {
+      if (data.session) router.replace("/dashboard");
+    });
+
+    const { data: listener } = supabase.auth.onAuthStateChange((event) => {
+      if (event === "SIGNED_IN") router.replace("/dashboard");
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, [router, supabase]);
+
   return (
-    <div>
-      <div>Hero</div>
-      <div>Current events</div>
-      <div>Call to action</div>
-    </div>
+    <Auth
+      supabaseClient={supabase}
+      appearance={{ theme: ThemeSupa }}
+      providers={["google"]}
+    />
   );
 }
+
+// export default function LoginPage() {
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         marginTop: "100px",
+//       }}
+//     >
+//       <h1>Sign in</h1>
+//       <GoogleSignInButton />
+//     </div>
+//   );
+// }
