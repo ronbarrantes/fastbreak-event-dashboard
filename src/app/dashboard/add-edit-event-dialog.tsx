0e0a4cb97ae3d0243f8bdcf7fad633b77364286a
Ron Barrantes <ronbarrantes@gmail.com>
2025-10-28 05:08:35 -0400

    WORKING PROGRESS

--- src/app/dashboard/add-edit-event-dialog.tsx
+++ src/app/dashboard/add-edit-event-dialog.tsx
@@ -1,29 +1,78 @@
 "use client";
 import { useState } from "react";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createInsertSchema, createUpdateSchema } from "drizzle-zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { SportEvent } from "@/types/types";
 import { tryCatch } from "@/utils/try-catch";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
 
-export function AddEditEventDialog({
+const eventSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title least 2 characters.",
+  }),
+
+  desc: z.string().min(2, {
+    message: "Description must be at least 2 characters.",
+  }),
+
+  name: z.string().min(3, {
+    message: "Name must be at least 3 characters.",
+  }),
+
+  sportType: z.string(),
+
+  date: z.date().nullish(),
+
+  description: z.string().min(2, {
+    message: "Description must be at least 2 characters.",
+  }),
+
+  venue: z.object({
+    name: z.string(),
+    id: z.uuid(),
+  }),
+});
+
+export const AddEditEventDialog = ({
   children,
   sportEvent,
 }: {
   children: React.ReactNode;
   sportEvent?: SportEvent;
-}) {
+}) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const form = useForm<z.infer<typeof eventSchema>>({
+    resolver: zodResolver(eventSchema),
+    defaultValues: {
+      name: sportEvent?.name ?? "",
+      sportType: sportEvent?.sportType ?? "",
+      date: sportEvent?.date ?? "",
+      description: sportEvent?.description ?? "",
+      venue: sportEvent?.venue ?? null,
+    },
+  });
+
   const dialogTitle = sportEvent ? "Edit event" : "Add event";
+
+  const handleSubmit = () => {};
 
   return (
     <Dialog onOpenChange={setIsOpen} open={isOpen}>
@@ -56,7 +105,65 @@ export function AddEditEventDialog({
         <DialogClose asChild>
           <Button type="button">Close</Button>
         </DialogClose>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex h-full flex-col space-y-8"
+          >
+            {id ? (
+              <DialogHeader>
+                <DialogTitle>Update the now item</SheetTitle>
+                <DialogDescription>What is your update:</SheetDescription>
+              </DialogHeader>
+            ) : (
+              <DialogHeader>
+                <DialogTitle>Add a now item</SheetTitle>
+                <DialogDescription>
+                  What am you up to right now:
+                </DialogDescription>
+              </DialogHeader>
+            )}
+            <div className="flex flex-1 auto-rows-min flex-col gap-6 px-4">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="title..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="desc"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="description..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="submit">Save changes</Button>
+              </DialogClose>
+              <DialogClose asChild>
+                <Button variant="outline">Close</Button>
+              </DialogClose>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
-}
+};
--- src/app/dashboard/columns.tsx
+++ src/app/dashboard/columns.tsx
@@ -12,12 +12,6 @@ import {
 } from "@/components/ui/popover";
 import { SportEvent } from "@/types/types";
 import { AddEditEventDialog } from "./add-edit-event-dialog";
-export type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
-};
 
 export const columns: ColumnDef<SportEvent>[] = [
   { accessorKey: "name", header: "Event Name" },
