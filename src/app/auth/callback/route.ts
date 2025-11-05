711a448eb717a7fdbb1ea127ca10e9018a951f9d
Ron Barrantes <ronbarrantes@gmail.com>
2025-11-04 21:34:16 -0500

    Change the callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,26 +1,62 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
+// import { createClient } from "@/utils/supabase/server";
+
+// export async function GET(request: Request) {
+//   const { searchParams } = new URL(request.url);
+
+//   const errorDescription = searchParams.get("error_description");
+//   if (errorDescription) {
+//     redirect(`/login?error=${encodeURIComponent(errorDescription)}`);
+//   }
+
+//   const code = searchParams.get("code");
+//   if (!code) {
+//     redirect("/login");
+//   }
+
+//   const supabase = await createClient();
+//   const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+//   if (error) {
+//     redirect("/error");
+//   }
+
+//   redirect("/dashboard");
+// }
+
+import { NextResponse } from "next/server";
+
+// The client you created from the Server-Side Auth instructions
 import { createClient } from "@/utils/supabase/server";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-
-  const errorDescription = searchParams.get("error_description");
-  if (errorDescription) {
-    redirect(`/login?error=${encodeURIComponent(errorDescription)}`);
-  }
-
+  const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  if (!code) {
-    redirect("/login");
+  // if "next" is in param, use it as the redirect URL
+  let next = searchParams.get("next") ?? "/";
+  if (!next.startsWith("/")) {
+    // if "next" is not a relative URL, use the default
+    next = "/";
   }
 
-  const supabase = await createClient();
-  const { error } = await supabase.auth.exchangeCodeForSession(code);
-
-  if (error) {
-    redirect("/error");
+  if (code) {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+      const isLocalEnv = process.env.NODE_ENV === "development";
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
+    }
   }
 
-  redirect("/dashboard");
+  // return the user to an error page with instructions
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
