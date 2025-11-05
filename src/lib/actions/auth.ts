e9169339a22384de321a3bec24a202b0a91606fb
Ron Barrantes <ronbarrantes@gmail.com>
2025-11-04 20:05:36 -0500

    Change the getURL

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -29,15 +29,27 @@ async function resolveRedirectUrl(pathname: string) {
   );
 }
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url;
+};
+
 export async function signInWithGoogle() {
   const supabase = await createClient();
 
-  const redirectTo = await resolveRedirectUrl("/auth/callback");
+  // const redirectTo = await resolveRedirectUrl("/auth/callback");
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo,
+      redirectTo: getURL(),
     },
   });
 
